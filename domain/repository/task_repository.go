547f21ea66524ac2bb0cf65e3f4c975800215dc4
Arin Alexia <arinalexia@student.ub.ac.id>
2025-11-21 23:24:12 +0700

    Refactor TaskRepository to include context and CRUD methods

--- domain/repository/task_repository.go
+++ domain/repository/task_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"github.com/Candrandika/be-todo-app-hmdtif/domain/entity"
 	"gorm.io/gorm"
+	"context"
 )
 
 type TaskRepository interface {
@@ -31,4 +32,11 @@ func (r *taskRepository) GetAll() ([]entity.Task, error) {
 
 func (r *taskRepository) CreateNew(task *entity.Task) error {
 	return r.db.Create(task).Error
+}
+type TaskRepository interface {
+	Create(ctx context.Context, t entity.Task) (entity.Task, error)
+	GetAll(ctx context.Context) ([]entity.Task, error)
+	GetByID(ctx context.Context, id uint) (entity.Task, error)
+	Update(ctx context.Context, t entity.Task) (entity.Task, error)
+	Delete(ctx context.Context, id uint) error
 }
