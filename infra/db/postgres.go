5b36f4acb6dbd35f6c2c728890bba25a7a9df540
Arin Alexia <arinalexia@student.ub.ac.id>
2025-11-21 23:30:19 +0700

    Implement task repository with CRUD methods
    Added task repository implementation with methods for GetByID, Update, and Delete.

--- infra/db/postgres.go
+++ infra/db/postgres.go
@@ -8,6 +8,8 @@ import (
 	"github.com/Candrandika/be-todo-app-hmdtif/domain/entity"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"context"
+	"errors"
 )
 
 func NewPostgresDatabase(env *config.Env) *gorm.DB {
@@ -35,4 +37,40 @@ func Migrate(db *gorm.DB) {
 		log.Fatalf("Failed to auto migrate database: %v", err)
 	}
 	log.Println("Database migration completed")
+}
+type taskRepository struct {
+	db *gorm.DB
+}
+
+func NewTaskRepository(db *gorm.DB) *taskRepository {
+	return &taskRepository{db: db}
+}
+
+func (r *taskRepository) GetByID(ctx context.Context, id uint) (entity.Task, error) {
+	var t entity.Task
+	if err := r.db.WithContext(ctx).First(&t, id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return entity.Task{}, err
+		}
+		return entity.Task{}, err
+	}
+	return t, nil
+}
+
+func (r *taskRepository) Update(ctx context.Context, t entity.Task) (entity.Task, error) {
+	if err := r.db.WithContext(ctx).Save(&t).Error; err != nil {
+		return entity.Task{}, err
+	}
+	return t, nil
+}
+
+func (r *taskRepository) Delete(ctx context.Context, id uint) error {
+	res := r.db.WithContext(ctx).Delete(&entity.Task{}, id)
+	if res.Error != nil {
+		return res.Error
+	}
+	if res.RowsAffected == 0 {
+		return gorm.ErrRecordNotFound
+	}
+	return nil
 }
